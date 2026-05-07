48829bf64569eb35808fbf713952a591f7ce24c2
Meenu <shubhampanwar426@gmail.com>
2026-05-07 23:34:36 +0530

    change in navbar

--- bakingwebsite/src/components/navbar.jsx
+++ bakingwebsite/src/components/navbar.jsx
@@ -2,7 +2,7 @@
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-6 bg-orange-50 shadow-sm position: fixed w-full z-10">
-      <h1 className="text-2xl font-serif font-bold text-orange-700">Khosla Bites Bakery</h1>
+      <h1 className="text-2xl font-serif font-bold text-orange-900">Khosla Bites Bakery</h1>
       <div className="space-x-6 font-medium text-gray-600">
         <a href="#" className="hover:text-orange-500">Menu</a>
         <a href="#" className="hover:text-orange-500">Our Story</a>
